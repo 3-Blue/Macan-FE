f46dba904661180554f59fe52ae2dd762227cc6b
NilaRahimi <nilarahimi82@gmail.com>
2026-07-18 15:47:03 +0330

    feat: add featured projects carousel/scroll section (#10)

---
+++ components/featured-projects-carousel.tsx
@@ -0,0 +1,5 @@
+import FeaturedProjectsCarousel from "@/components/FeaturedProjectsCarousel";
+
+// once #29 (projects CMS model) lands, fetch real data and pass it in:
+// <FeaturedProjectsCarousel projects={projectsFromCms} />
+<FeaturedProjectsCarousel />
