0b66f7a7f062b19fbcbf0303a8cb38f72b338124
NilaRahimi <nilarahimi82@gmail.com>
2026-07-20 12:50:56 +0330

    feat: add stats section to homepage alongside hero

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,12 @@
 import { Hero } from "@/components/sections/Hero";
+import { StatsSection } from "@/components/sections/StatsSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <StatsSection />
     </>
   );
 }
+
